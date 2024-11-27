eabb80088089f3af32c65f184442dcd3f19fd248
Ramirez474747 <114956756+fernando45587652@users.noreply.github.com>
2024-11-27 13:51:30 -0600

    subiendo modelo Solicitud_log

---
+++ app/Models/Solicitud.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Solicitud extends Model
+{
+    use  HasFactory; 
+    protected $table = 'solicitud_log';
+ protected $primaryKey = 'slog_id';
+	public $timestamps = false;
+ 
+ protected $fillable = [
+    'slog_id_emp',
+    'slog_accion',
+    'slog_cod_af',
+    'slog_comentario',
+    'slog_codigo_af',
+     
+];   
+public function employee()
+	{
+		return $this->belongsTo(EmpleadoModelo::class, 'slog_id_emp', 'idemployee');
+	}
+   /* public function activo()
+	{
+		return $this->belongsTo(afActivoModelo::class, 'slog_cod_af', 'id_activo');
+	}*/
+
+}
+
+
+
+
+
+ 
